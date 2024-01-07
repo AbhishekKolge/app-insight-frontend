5765a4798e5d4bfbd193b1059dec762d37ca5c5a
Abhishek Kolge <83946567+AbhishekKolge@users.noreply.github.com>
2024-01-08 01:29:07 +0530

    Update page.js

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -61,7 +61,6 @@ const Login = (props) => {
         .then((data) => {
           loginFormik.resetForm();
           dispatch(loginHandler(data));
-          router.replace('/dashboard');
         })
         .catch((error) => {
           if (error.status === 403) {

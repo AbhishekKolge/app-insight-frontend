87bee692d353cce81583935d546393b7d5b385e6
Abhishek Kolge <83946567+AbhishekKolge@users.noreply.github.com>
2024-02-15 14:58:58 +0530

    Update data-table-toolbar.jsx

--- src/components/ui/data-table-toolbar.jsx
+++ src/components/ui/data-table-toolbar.jsx
@@ -32,44 +32,46 @@ export function DataTableToolbar({
     type.length;
 
   return (
-    <div className='flex items-center justify-between overflow-x-scroll'>
-      <div className='flex flex-1 items-center space-x-2'>
+    <div className='flex items-center justify-between'>
+      <div className='w-full flex flex-1 items-center space-x-2'>
         <Input
           placeholder='Search...'
           value={searchValue}
           onChange={onSearch}
           className='text-xs h-8 w-[150px] lg:w-[250px]'
         />
-        <CategorySelector
+        <div className="overflow-x-scroll flex items-center space-x-2">
+          <CategorySelector
           onSelect={addCategory}
           selected={categoryId}
           onClear={clearCategory}
-        />
-        <GenreSelector
-          onSelect={addGenre}
-          selected={genreId}
-          onClear={clearGenre}
-        />
-        <ContentRatingSelector
-          onSelect={addContentRating}
-          selected={contentRatingId}
-          onClear={clearContentRating}
-        />
-        <TypeSelector
-          setTypeHandler={setTypeHandler}
-          clearTypeHandler={clearTypeHandler}
-          type={type}
-        />
-        {!!isFiltered && (
-          <Button
-            variant='rating'
-            onClick={clearAllFilters}
-            className='h-8 px-2 lg:px-3 text-xs'
-          >
-            Reset
-            <Cross2Icon className='ml-2 h-4 w-4' />
-          </Button>
-        )}
+          />
+          <GenreSelector
+            onSelect={addGenre}
+            selected={genreId}
+            onClear={clearGenre}
+          />
+          <ContentRatingSelector
+            onSelect={addContentRating}
+            selected={contentRatingId}
+            onClear={clearContentRating}
+          />
+          <TypeSelector
+            setTypeHandler={setTypeHandler}
+            clearTypeHandler={clearTypeHandler}
+            type={type}
+          />
+          {!!isFiltered && (
+            <Button
+              variant='rating'
+              onClick={clearAllFilters}
+              className='h-8 px-2 lg:px-3 text-xs'
+            >
+              Reset
+              <Cross2Icon className='ml-2 h-4 w-4' />
+            </Button>
+          )}
+        </div>
       </div>
     </div>
   );
